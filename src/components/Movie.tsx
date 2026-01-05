a8b405c5dbf11b5cea3c6df24472ad25a9ea6cb2
BelalHashem007 <belal.hashem.saleh@gmail.com>
2026-01-06 00:31:23 +0200

    Fixing a bug with img layout

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -10,12 +10,12 @@ export interface Movie {
 
 export default function Movie({ movie }: { movie: Movie }) {
   const imgSrc = "https://image.tmdb.org/t/p/w342" + movie.img_url;
-    const vote = Number(movie.vote_average).toFixed(1)
-    const dateFormatted = format(movie.release_date,"MMM dd yyyy")
+  const vote = Number(movie.vote_average).toFixed(1);
+  const dateFormatted = format(movie.release_date, "MMM dd yyyy");
 
   return (
     <div>
-      <a href="">
+      <a href="" className="aspect-2/3 block">
         <img
           src={imgSrc}
           loading="lazy"
@@ -27,7 +27,10 @@ export default function Movie({ movie }: { movie: Movie }) {
         <a href="" title={movie.title}>
           <h3 className="font-bold text-2xl">{movie.title}</h3>
         </a>
-        <div aria-label="Movie Rating" className="w-8.75 absolute top-0 left-1 rounded-[50%] bg-emerald-600 text-white border p-1 -translate-y-10 text-center">
+        <div
+          aria-label="Movie Rating"
+          className="w-8.75 absolute top-0 left-1 rounded-[50%] bg-emerald-600 text-white border p-1 -translate-y-10 text-center"
+        >
           {vote}
         </div>
         <div className="text-gray-500">{dateFormatted}</div>
